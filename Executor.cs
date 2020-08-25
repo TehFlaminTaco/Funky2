bd20e12515e78d080944b0c62205b7c2e729ac70
William Lemon <william.lemon2@gmail.com>
2020-08-25 10:45:36 +1000

    Added a way to retreive command line arguments.

--- Executor.cs
+++ Executor.cs
@@ -16,7 +16,10 @@ namespace Funky
             var fileName = file is null ? "main.fnk" : new FunkyFile(file, "funky", ".fnk").shortName;
             Meta.GetMeta();
             try{
-                ExecuteProgram(code, fileName);
+                VarList argList = new VarList();
+                for(var i=0; i < args.Length; i++)
+                    argList.double_vars[i] = args[i];
+                ExecuteProgram(code, fileName, argList);
             }catch(System.Exception e){
                 System.Console.Error.WriteLine($"ERROR:\n{e.Message}");
             }
@@ -24,11 +27,13 @@ namespace Funky
 
         public static Regex CLEAN_ERROR = new Regex(@"\n(\n|\r|.)*");
         public static Regex SEMI_COLON = new Regex(@";");
-        public static Var ExecuteProgram(string code, string fileName){
+        public static Var ExecuteProgram(string code, string fileName, VarList cmdArgs = null){
             TExpression e;
             Var last = Var.nil;
             VarList scopeList = new VarList();
             scopeList.parent = Globals.get();
+            if(cmdArgs!=null)
+                scopeList.string_vars["arg"] = cmdArgs;
             Scope scope = new Scope(scopeList);
             StringClaimer claimer = new StringClaimer(code, fileName);
             while((e=TExpression.Claim(claimer))!=null){
